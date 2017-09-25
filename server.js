7bb8d63885120d928923857233dd5af84e7aabfd
System <none>
2017-09-25 18:33:57 +0000

    Remove client js to allow for file upload

--- server.js
+++ server.js
@@ -5,16 +5,30 @@
 let express = require('express');
 let app = express();
 let PORT = process.env.PORT || 8000
+let multer = require("multer");
+let bodyParser = require("body-parser");
+
+const upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use(express.static('public'));
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.render("index");
-  //response.sendFile(__dirname + '/views/index.html');
+});
+
+// post to get file metadata
+app.post("/filemetadata", upload.single("file"), (request, response) =>{
+  if(!request.body){
+    request.status(500).end();
+  }else{
+    response.json({ "size" : request.file.size })
+  }
 });
 
 
